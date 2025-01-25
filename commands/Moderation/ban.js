874da8fef19f89de9f5a4f6d894077fd2ba1e1ad
Luna K <me@lunashere.com>
2025-01-25 17:31:28 -0600

    Add ban command with user validation and moderation logging

---
+++ commands/Moderation/ban.js
@@ -0,0 +1,54 @@
+const { EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Bans a user')
+        .addUserOption(option => option.setName('user').setDescription('The user to ban').setRequired(true))
+        .addStringOption(option => option.setName('reason').setDescription('The reason for the ban').setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers),
+    async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        // Check if the user is trying to ban themselves
+        if (user.id === interaction.user.id) return interaction.reply({ content: 'You cannot ban yourself', ephemeral: true });
+        // Check if the user is trying to ban a bot
+        if (user.bot) return interaction.reply({ content: 'You cannot ban a bot', ephemeral: true });
+        const reason = interaction.options.getString('reason');
+
+        // check if someone is trying to ban a user with the same or higher role of themselves and is not the owner
+        if (interaction.guild.members.cache.get(user.id).roles.highest.position >= interaction.member.roles.highest.position && interaction.guild.ownerId !== interaction.user.id) {
+            return interaction.reply({ content: 'You cannot ban a user with the same or higher role than yourself', ephemeral: true });
+        }
+        
+        const channel = interaction.client.settingsManager.getGuildSettings(interaction.guild).modlogchannel;
+        if (channel) {
+            const modembed = new EmbedBuilder()
+                .setDescription(`**${user.username}#${user.discriminator}** (ID: ${user.id}) has been banned by **${interaction.user}** for **${reason}**`)
+                .setAuthor({ name: `${interaction.client.config.botname} Moderation`, iconURL: `${interaction.client.config.boticon}` })
+                .setColor(0xFF0000)
+                .setTimestamp();
+            channel.send({ embeds: [modembed] });
+        }
+
+        const userembed = new EmbedBuilder()
+            .setDescription(`You have been banned from **${interaction.guild.name}**.  If you feel this ban was unjust, please contact the server owner.`)
+            .setAuthor({ name: `${interaction.client.config.botname} Moderation`, iconURL: `${interaction.client.config.boticon}` })
+            .addFields({ name: 'Reason', value: reason, inline: true })
+            .setColor(0xFF0000)
+            .setTimestamp();
+        user.send({ embeds: [userembed] }).catch(err => console.log(err));
+
+        interaction.guild.members.ban(user, { reason: reason }).then(() => {
+            const embed = new EmbedBuilder()
+                .setDescription(`**${user}** has been banned.`)
+                .addFields({ name: 'Reason', value: reason, inline: true })
+                .setAuthor({ name: `${interaction.client.config.botname} Moderation`, iconURL: `${interaction.client.config.boticon}` })
+                .setColor(0xFF0000)
+                .setTimestamp();
+            interaction.reply({ embeds: [embed] });
+        }).catch(err => {
+            console.error(err);
+            return interaction.reply({ content: 'An error occurred while banning the user', ephemeral: true });
+        });
+    }
+}
