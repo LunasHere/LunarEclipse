0bb2d19e23eb3b8e2828bc52e27c7bb69c9a0272
Lunashere <lunaashere@gmail.com>
2025-05-02 23:05:45 -0500

    Refactor ban command to improve role validation and update moderation log message format

--- commands/Moderation/ban.js
+++ commands/Moderation/ban.js
@@ -15,16 +15,19 @@ module.exports = {
         if (user.bot) return interaction.reply({ content: 'You cannot ban a bot', flags: MessageFlags.Ephemeral });
         const reason = interaction.options.getString('reason');
 
-        // check if someone is trying to ban a user with the same or higher role of themselves and is not the owner
-        if (interaction.guild.members.cache.get(user.id).roles.highest.position >= interaction.member.roles.highest.position && interaction.guild.ownerId !== interaction.user.id) {
-            return interaction.reply({ content: 'You cannot ban a user with the same or higher role than yourself', flags: MessageFlags.Ephemeral });
+        const member = interaction.guild.members.cache.get(user.id);
+        if (member) {
+            if (member.roles.highest.position >= interaction.member.roles.highest.position 
+                && interaction.guild.ownerId !== interaction.user.id) {
+                return interaction.reply({ content: 'You cannot ban a user with the same or higher role than yourself', flags: MessageFlags.Ephemeral });
+            }
         }
         
         await interaction.client.settingsManager.getSettings(interaction.guild).then(settings => {
             if(settings.modlogchannel) {
                 const channel = interaction.guild.channels.cache.get(settings.modlogchannel);
                 const modembed = new EmbedBuilder()
-                    .setDescription(`**${user.username}#${user.discriminator}** (ID: ${user.id}) has been banned.`)
+                    .setDescription(`${user} (ID: ${user.id}) has been banned.`)
                     .setAuthor({ name: `${interaction.client.config.botname} Moderation`, iconURL: `${interaction.client.config.boticon}` })
                     .addFields({ name: 'Issued By', value: `${interaction.user}`, inline: true}, { name: 'Reason', value: reason, inline: true })
                     .setColor(0xFF0000)
